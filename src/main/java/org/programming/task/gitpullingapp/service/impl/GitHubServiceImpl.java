2620ebdcfee21cac309999176434d6486db17513
BolgovID <xulihax@gmail.com>
2024-06-01 23:20:13 +0300

    doc(task) - add method contracts

--- src/main/java/org/programming/task/gitpullingapp/service/impl/GitHubServiceImpl.java
+++ src/main/java/org/programming/task/gitpullingapp/service/impl/GitHubServiceImpl.java
@@ -17,6 +17,16 @@ public class GitHubServiceImpl implements GitHubService {
     private final GitHubApiService gitHubApiService;
     private final GitHubMapper gitHubMapper;
 
+    /**
+     * The getUserNotForkedRepositories method retrieves all
+     * repositories of a given user that are not forks, and maps them
+     * to a DTO containing repository details and their branches.
+     *
+     * @param username - name of git user
+     * @return Flux<UserRepositoryDto>
+     * @throws org.programming.task.gitpullingapp.exception.GitUserNotFoundException if the user not found
+     * @throws org.programming.task.gitpullingapp.exception.ApiRateLimitExceededException if the API limit exceeded
+     */
     @Override
     public Flux<UserRepositoryDto> getUserNotForkedRepositories(String username) {
         return gitHubApiService
@@ -25,6 +35,16 @@ public class GitHubServiceImpl implements GitHubService {
                 .flatMap(this::getBranchesAndMapToDto);
     }
 
+    /**
+     * The getAllRepositoryBranches method retrieves all branches of a specified repository for
+     * a given user and maps them to BranchDto objects.
+     *
+     * @param username - name of git user
+     * @param repository - git repository name
+     * @return Flux<BranchDto>
+     * @throws org.programming.task.gitpullingapp.exception.GitUserNotFoundException if the user repository not found
+     * @throws org.programming.task.gitpullingapp.exception.ApiRateLimitExceededException if the API limit exceeded
+     */
     @Override
     public Flux<BranchDto> getAllRepositoryBranches(String username, String repository) {
         return gitHubApiService.pullBranches(username, repository)
